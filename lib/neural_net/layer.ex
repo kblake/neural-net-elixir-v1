f6c9addbe9413f81d83e91dc390f1ddb16c1b901
Karmen Blake <dudeblake@gmail.com>
2016-01-06 10:28:07 -0800

    refactor so that setting neurons is easier

--- lib/neural_net/layer.ex
+++ lib/neural_net/layer.ex
@@ -15,6 +15,11 @@ defmodule NeuralNet.Layer do
     Agent.update(layer_name, &(&1 -- &1))
   end
 
+  defp set_neurons(layer_name, neurons) do
+    clear_neurons(layer_name)
+    add_neurons(layer_name, neurons)
+  end
+
   def activate(layer_name, values \\ nil) do
     values = values || []
 
@@ -43,10 +48,7 @@ defmodule NeuralNet.Layer do
     end
 
     input_layer_neurons = build_input_layer_neurons_with_connections(input_layer_name, output_layer_name)
-    clear_neurons(:input_layer)
-    add_neurons(input_layer_name, input_layer_neurons)
-
-
+    set_neurons(input_layer_name, input_layer_neurons)
     stop_agent(:source_neurons)
 
     {:ok, input_layer_neurons, []}
@@ -65,12 +67,11 @@ defmodule NeuralNet.Layer do
       end
     end
 
-    updated_input_neurons =
-      NeuralNet.Layer.neurons(input_layer_name)
-      |> Stream.with_index
-      |> Enum.map(fn tuple ->
-        {neuron, index} = tuple
-        %NeuralNet.Neuron{neuron | outgoing: Enum.at(input_layer_outgoing_connections, index)}
-      end)
+    NeuralNet.Layer.neurons(input_layer_name)
+    |> Stream.with_index
+    |> Enum.map(fn tuple ->
+      {neuron, index} = tuple
+      %NeuralNet.Neuron{neuron | outgoing: Enum.at(input_layer_outgoing_connections, index)}
+    end)
   end
 end
