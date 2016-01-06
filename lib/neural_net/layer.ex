32cadbc02338996b475289e04baee29f935d12ec
Karmen Blake <dudeblake@gmail.com>
2016-01-06 08:53:39 -0800

    input layer's outgoing connections updated

--- lib/neural_net/layer.ex
+++ lib/neural_net/layer.ex
@@ -1,5 +1,4 @@
 defmodule NeuralNet.Layer do
-
   def start_link(layer_name, neurons \\ []) do
     Agent.start_link(fn -> neurons end, name: layer_name)
   end
@@ -10,6 +9,10 @@ defmodule NeuralNet.Layer do
 
   def add_neurons(layer_name, neurons) do
     Agent.update(layer_name, &(&1 ++ neurons))
+  end
+
+  def clear_neurons(layer_name) do
+    Agent.update(layer_name, &(&1 -- &1))
   end
 
   def activate(layer_name, values \\ nil) do
@@ -23,27 +26,51 @@ defmodule NeuralNet.Layer do
         NeuralNet.Neuron.activate(neuron, Enum.at(values, index))
       end)
     end)
+
+    neurons(layer_name)
   end
 
+  def connect(input_layer_name, output_layer_name) do
+    Agent.start_link(fn -> [] end, name: :source_neurons)
+    #Agent.start_link(fn -> [] end, name: :target_neurons)
+
+    Enum.each NeuralNet.Layer.neurons(input_layer_name), fn(source) ->
+      Enum.each NeuralNet.Layer.neurons(output_layer_name), fn(target) ->
+        {:ok, s, t} = NeuralNet.Neuron.connect(source, target)
+        add_neurons(:source_neurons, [s])
+        #add_neurons(:target_neurons, [t])
+      end
+    end
+
+    input_layer_neurons = build_input_layer_neurons_with_connections(input_layer_name, output_layer_name)
+    clear_neurons(:input_layer)
+    add_neurons(input_layer_name, input_layer_neurons)
 
 
+    stop_agent(:source_neurons)
 
-  #defstruct neurons: []
+    {:ok, input_layer_neurons, []}
+  end
 
-  #def activate(layer, values \\ nil) do
-    #values = values || []
+  defp stop_agent(agent_name) do
+    Process.exit(Process.whereis(agent_name), :shutdown)
+  end
 
-    #activated_neurons = layer.neurons
-                        #|> Stream.with_index
-                        #|> Enum.map(fn(tuple) ->
-                                        #{neuron, index} = tuple
-                                        #NeuralNet.Neuron.activate(neuron, values[index])
-                                     #end)
+  defp build_input_layer_neurons_with_connections(input_layer_name, output_layer_name) do
+    neurons_with_connections = Enum.chunk(neurons(:source_neurons), length(NeuralNet.Layer.neurons(output_layer_name)))
 
-    #{:ok, %NeuralNet.Layer{neurons: activated_neurons}}
-  #end
+    input_layer_outgoing_connections = Enum.map neurons_with_connections, fn(neurons) ->
+      Enum.map neurons, fn neuron ->
+        List.first neuron.outgoing
+      end
+    end
 
-  ##def connect(target_layer) do
-
-  ##end
+    updated_input_neurons =
+      NeuralNet.Layer.neurons(input_layer_name)
+      |> Stream.with_index
+      |> Enum.map(fn tuple ->
+        {neuron, index} = tuple
+        %NeuralNet.Neuron{neuron | outgoing: Enum.at(input_layer_outgoing_connections, index)}
+      end)
+  end
 end
--- test/neural_net/layer_test.exs
+++ test/neural_net/layer_test.exs
@@ -3,69 +3,63 @@ defmodule NeuralNet.LayerTest do
   doctest NeuralNet.Layer
 
   test "defaults layer" do
-    NeuralNet.Layer.start_link(:input_layer) 
+    NeuralNet.Layer.start_link(:input_layer)
     assert NeuralNet.Layer.neurons(:input_layer) == []
   end
 
   test "layer initialized with neurons" do
     neurons = [%NeuralNet.Neuron{input: 1}, %NeuralNet.Neuron{input: 2}]
-    NeuralNet.Layer.start_link(:input_layer, neurons) 
+    NeuralNet.Layer.start_link(:input_layer, neurons)
     assert NeuralNet.Layer.neurons(:input_layer) == neurons
   end
 
   test "add neurons to layer" do
     neurons = [%NeuralNet.Neuron{input: 1}, %NeuralNet.Neuron{input: 2}]
-    NeuralNet.Layer.start_link(:input_layer) 
+    NeuralNet.Layer.start_link(:input_layer)
     NeuralNet.Layer.add_neurons(:input_layer, neurons)
     assert NeuralNet.Layer.neurons(:input_layer) == neurons
   end
 
+  test "clear neurons from layer" do
+    neurons = [%NeuralNet.Neuron{input: 1}, %NeuralNet.Neuron{input: 2}]
+    NeuralNet.Layer.start_link(:input_layer, neurons)
+    NeuralNet.Layer.clear_neurons(:input_layer)
+    assert NeuralNet.Layer.neurons(:input_layer) == []
+  end
+
   test "activate when values are nil" do
     neurons = [%NeuralNet.Neuron{input: 1}, %NeuralNet.Neuron{input: 2}]
-    NeuralNet.Layer.start_link(:input_layer, neurons) 
-    NeuralNet.Layer.activate(:input_layer) 
-    Enum.each NeuralNet.Layer.neurons(:input_layer), fn neuron ->
+    NeuralNet.Layer.start_link(:input_layer, neurons)
+    Enum.each NeuralNet.Layer.activate(:input_layer), fn neuron ->
       assert neuron.output == 0.5
     end
   end
 
   test "activate with values" do
     neurons = [%NeuralNet.Neuron{input: 1}, %NeuralNet.Neuron{input: 2}]
-    NeuralNet.Layer.start_link(:input_layer, neurons) 
-    NeuralNet.Layer.activate(:input_layer, [1,2]) 
-    Enum.each NeuralNet.Layer.neurons(:input_layer), fn neuron ->
+    NeuralNet.Layer.start_link(:input_layer, neurons)
+    Enum.each NeuralNet.Layer.activate(:input_layer, [1,2]), fn neuron ->
       assert neuron.output >= 0.0 && neuron.output <= 1.0
     end
   end
-  
 
+  test "input layer's outgoing neurons are stored" do
+    input_neurons = [%NeuralNet.Neuron{input: 1}, %NeuralNet.Neuron{input: 2}]
+    NeuralNet.Layer.start_link(:input_layer, input_neurons)
+    output_neurons = [%NeuralNet.Neuron{input: 3}, %NeuralNet.Neuron{input: 4}, %NeuralNet.Neuron{input: 5}]
+    NeuralNet.Layer.start_link(:output_layer, output_neurons)
 
-  #it "with values" do
-    #layer.activate([1,2])
-    #layer.neurons.each do |neuron|
-    #expect(0.5..1).to cover(neuron.output)
-    #end
-  #end
+    #NeuralNet.Layer.connect(:input_layer, :output_layer)
 
+    {:ok, input_layer_neurons, output_layer_neurons} = NeuralNet.Layer.connect(:input_layer, :output_layer)
 
-  #test "has default values" do
-    #layer = %NeuralNet.Layer{}
-    #assert layer.neurons == []
-  #end
-
-  #test "input layer's outgoing neurons are stored" do
-    #input_layer  = %NeuralNet.Layer{neurons: [%NeuralNet.Neuron{input: 1}, %NeuralNet.Neuron{input: 2}]}
-    #output_layer = %NeuralNet.Layer{neurons: [%NeuralNet.Neuron{input: 3}, %NeuralNet.Neuron{input: 4}]}
-
-    #{:ok, input_layer, output_layer} = NeuralNet.Layer.connect(input_layer, output_layer)
-
-    #Enum.each input_layer.neurons, fn(neuron) ->
-      #assert length(neuron.outgoing) == 2
-      #assert length(neuron.incoming) == 0
-      ##target_neurons = Enum.map neuron.outgoing, fn(connection) -> connection.target end
-      ##assert target_neurons == output_layer.neurons
-    #end
-  #end
+    Enum.each input_layer_neurons, fn(neuron) ->
+      assert length(neuron.outgoing) == length(output_neurons)
+      assert length(neuron.incoming) == 0
+      #target_neurons = Enum.map neuron.outgoing, fn(connection) -> connection.target end
+      #assert target_neurons == output_layer.neurons
+    end
+  end
 
   #it "output layer's incoming neurons + bias neuron are stored" do
     #input_layer.connect(output_layer)
